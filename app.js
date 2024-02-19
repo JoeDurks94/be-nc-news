8edd9741b6f917eda2b5dc2a6a4a5d560920d4d3
Joe Durkan <durkanjm01@hotmail.co.uk>
2024-02-19 15:14:43 +0000

    completed task 3 - also implemented error handling from last task - for sad path

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("./controller/app.controller.js");
+const { getTopics, getAPI } = require("./controller/app.controller.js");
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+
+app.get("/api", getAPI)
 
 app.use((error, request, response, next) => {
   next(error);
