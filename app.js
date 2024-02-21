c3a417484bdb89e9218af9046d9eabc2d292156a
Joe Durkan <durkanjm01@hotmail.co.uk>
2024-02-21 09:42:33 +0000

    task 6 completed

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const {
-  getTopics,
-  getAPI,
-  getArticleById,
-  getAllArticles,
+	getTopics,
+	getAPI,
+	getArticleById,
+	getAllArticles,
+	getCommentsByArticleId,
 } = require("./controller/app.controller.js");
 
 app.get("/api/topics", getTopics);
@@ -14,15 +15,17 @@ app.get("/api", getAPI);
 
 app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles", getAllArticles)
+app.get("/api/articles", getAllArticles);
+
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.use((error, request, response, next) => {
-  if (error.status === 404) {
-    response.status(404).send();
-  }
-  if(error.code === '22P02'){
-    response.status(400).send()
-  }
+	if (error.status === 404) {
+		response.status(404).send();
+	}
+	if (error.code === "22P02") {
+		response.status(400).send();
+	}
 });
 
 module.exports = app;
--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,12 +1,13 @@
 const {
-	getAllTopics,
+	fetchAllTopics,
 	fetchArticleById,
 	fetchAllArticles,
+	fetchCommentsByArticleId,
 } = require("../model/app.model");
 const allEndpoints = require("../endpoints.json");
 
 function getTopics(request, response, next) {
-	getAllTopics().then((topicsArray) => {
+	fetchAllTopics().then((topicsArray) => {
 		response.status(200).send({ topics: topicsArray });
 	});
 }
@@ -35,4 +36,31 @@ function getAllArticles(request, response, next) {
 		});
 }
 
-module.exports = { getTopics, getAPI, getArticleById, getAllArticles };
+function getCommentsByArticleId(request, response, next) {
+	const lookUpArticleId = request.params.article_id;
+	const promises = [
+		fetchCommentsByArticleId(lookUpArticleId),
+		fetchArticleById(lookUpArticleId),
+	];
+	Promise.all(promises)
+		.then((comments) => {
+			commentsArray = comments[0];
+			if (commentsArray.length === 0) {
+				response.status(200).send({
+					msg: "there any no comments for the supplied acticle",
+				});
+			}
+			response.status(200).send({ comments: commentsArray });
+		})
+		.catch((error) => {
+			next(error);
+		});
+}
+
+module.exports = {
+	getCommentsByArticleId,
+	getTopics,
+	getAPI,
+	getArticleById,
+	getAllArticles,
+};
