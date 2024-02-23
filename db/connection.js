522756ae07d194a55e64fde6055ec37afc88707d
Joe Durkan <durkanjm01@hotmail.co.uk>
2024-02-23 16:15:41 +0000

    change port

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,18 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+	path: `${__dirname}/../.env.${ENV}`,
+});
 
-const config = {}
+const config = {};
 
 if (ENV === "production") {
 	config.connectionString = process.env.DATABASE_URL;
 	config.max = 2;
 }
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
+	throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
